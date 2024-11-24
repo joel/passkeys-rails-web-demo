27b0b645c5a26524e81e5cc19951ae7101bcbb6b
Joel AZEMAR <joel.azemar@gmail.com>
2024-11-24 10:30:20 +0100

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@
 # Source: https://github.com/rails/rails/blob/7-1-stable/activerecord/lib/rails/generators/active_record/model/templates/model.rb.tt
 class ::Post < ApplicationRecord
   belongs_to :user
+
+  attribute :user_id, :uuid_v7
 end
