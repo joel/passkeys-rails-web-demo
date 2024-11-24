9a1d53f9e4bf368216e5b68599435e5aa86253d2
Joel AZEMAR <joel.azemar@gmail.com>
2024-11-24 10:31:11 +0100

    Add validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@
 # Source: https://github.com/rails/rails/blob/7-1-stable/activerecord/lib/rails/generators/active_record/model/templates/model.rb.tt
 class ::Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
 
   attribute :user_id, :uuid_v7
 end
